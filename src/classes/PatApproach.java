4ed66f89ecfeaf3eb62a85c0f25bca5033b9281f
Jose A. Melendez <jose.melendez35@upr.edu>
2017-12-06 22:40:11 -0400

    Pat Approach: fixed problem that removed disappointed customers from processing queue.

--- src/classes/PatApproach.java
+++ src/classes/PatApproach.java
@@ -6,6 +6,7 @@ import tools.SLLQueue;
 public class PatApproach<E> {
 
 	private int time;
+	private int orderNumber = 0;
 	private double profit = 0;
 	private int numberOfDisappointedCustomers = 0;
 	ArrayIndexList<Customer> inputCustomers;
@@ -21,17 +22,21 @@ public class PatApproach<E> {
 	public void processCustomer() {
 		time=0;
 		while(!inputCustomers.isEmpty() || !processQueue.isEmpty()) {
+			System.out.println("Time: " + time);
 			if(!processQueue.isEmpty()) {
 				processQueue.first().setTimeToPrepare(processQueue.first().getTimeToPrepare()-1);
 				System.out.println("Remaining time to prepare order: " + processQueue.first().getTimeToPrepare());
 				
 				if(processQueue.first().getTimeToPrepare()==0) {
+					orderNumber++;
 					profit+=processQueue.dequeue().getCostOfOrder();
-					System.out.println("Order finished!\n");
+					System.out.println("Order " + orderNumber+  " finished!\n");
 					
-					if(!processQueue.isEmpty() && time - processQueue.first().getArrivalTime()> processQueue.first().getLevelOfPatience()) {
+					while(!processQueue.isEmpty() && time - processQueue.first().getArrivalTime()> processQueue.first().getLevelOfPatience()) {
+						orderNumber++;
 						processQueue.dequeue();	
 						numberOfDisappointedCustomers++;
+						System.out.println("Order " + orderNumber + " dissapointed\n");
 					}
 				}
 				
--- src/classes/Restaurant.java
+++ src/classes/Restaurant.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 import tools.ArrayIndexList;
 
 public class Restaurant {
-	private int numCustomers = 0;
 	private ArrayIndexList<Customer> listOfCustomers = new ArrayIndexList<Customer>();
 	
 	public Restaurant() {
@@ -15,14 +14,14 @@ public class Restaurant {
 	
 	public static void main(String[] args) {
 		Restaurant gallimbo = new Restaurant();
-		File file = new File("another.csv");
+		File file = new File("input3.csv");
 		gallimbo.readFile(file);
 		
 		PatApproach<Customer> pat = new PatApproach<Customer>(gallimbo.getListOfCustomers());
 		pat.processCustomer();
 		
 		System.out.println("Pat approach disappointed customers: " + pat.getNumberOfDisappointedCustomers());
-		System.out.println("Pat approach profit: $" + pat.getProfit());
+		System.out.printf("Pat approach profit: $%.2f", pat.getProfit());
 	}
 
 	public void readFile(File file) {
@@ -30,10 +29,9 @@ public class Restaurant {
 			Scanner in = new Scanner(file);
 
 			while(in.hasNext()) {
-				numCustomers++;
 				String data = in.nextLine();
 				String[] separatedData = data.split(",");
-				separatedData[3] = separatedData[3].substring(1, separatedData[3].length()-1);
+				separatedData[3] = separatedData[3].substring(1, separatedData[3].length());
 				Customer currentCustomer = new Customer(Integer.parseInt(separatedData[0]),Integer.parseInt(separatedData[1]),
 						Integer.parseInt(separatedData[2]), 
 						Double.parseDouble(separatedData[3]),
